27655805f2941967b24dd77428abfc9daf1b732f
aadamsx <aadams99@gmail.com>
2017-01-03 18:55:33 -0600

    removed packages and dependencies

--- imports/vuex/store.js
+++ imports/vuex/store.js
@@ -1,1 +1,8 @@
-import {StoreModule} from 'meteor/akryum:vuex';
+// import {StoreModule} from 'meteor/akryum:vuex';
+//
+// const root = new StoreModule();
+//
+// // Add some initial state
+// root.addState({
+//   name: 'world'
+// });
