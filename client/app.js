2919717616d16faf261e6cdad63ab9ec1704e20d
aadamsx <aadams99@gmail.com>
2017-01-04 10:47:37 -0600

    changed router to native

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 import '/imports/client/router.js';
-// import { Vue } from 'meteor/akryum:vue';
 import Vue from 'vue';
 import VueMeteorTracker from 'vue-meteor-tracker'
-import { Router, nativeScrollBehavior } from 'meteor/akryum:vue-router2';
+// import { Router, nativeScrollBehavior } from 'meteor/akryum:vue-router2';
+import Router from 'vue-router'
 import AppLayout from '/imports/client/views/AppLayout.vue';
 
 const router = new Router({
@@ -11,10 +11,11 @@ const router = new Router({
 });
 
 Meteor.startup(() => {
+  Vue.use(VueMeteorTracker);
+  Vue.use(Router);
+
   new Vue({
       router: router.start(),
       render: h => h(AppLayout),
   }).$mount('app');
-
-  Vue.use(VueMeteorTracker);
 });
--- imports/client/router.js
+++ imports/client/router.js
@@ -1,4 +1,6 @@
-import { Router } from 'meteor/akryum:vue-router2';
+// import { Router } from 'meteor/akryum:vue-router2';
+import Router from 'vue-router'
+
 import Index from '/imports/client/views/public/Index.vue';
 import Page from '/imports/client/views/public/Page.vue';
 import FirstForm from '/imports/client/views/public/FirstForm.vue';
@@ -6,30 +8,54 @@ import FirstForm from '/imports/client/views/public/FirstForm.vue';
 // Not found
 import NotFound from '/imports/client/views/public/NotFound.vue';
 
-Router.configure(router => {
-  router.addRoutes([{
-    path: "/",
-    name: "Index",
-    components: {
-      default: Index,
+Router.map({
+  // Not found handler
+  '*': {
+    component: {
+        template:
+        '<div>' +
+        '<h1>Not Found</h1>' +
+        '</div>'
     }
-  }, {
-    path: "/page",
-    name: "Page",
-    components: {
-      default: Page,
-    }
-  }, {
-    path: "/first-form",
-    name: "FirstForm",
-    components: {
-      default: FirstForm,
-    }
-  }, {
-    path: "/not-found",
-    name: "NotFound",
-    components: {
-      default: NotFound,
-    }
-  },]);
+  },
+  '/': {
+    component: Index
+  },
+  '/page': {
+    component: Page
+  },
+  '/first-form': {
+    component: FirstForm
+  },
+  '/not-found': {
+    component: NotFound
+  }
 });
+
+// Router.configure(router => {
+//   router.addRoutes([{
+//     path: "/",
+//     name: "Index",
+//     components: {
+//       default: Index,
+//     }
+//   }, {
+//     path: "/page",
+//     name: "Page",
+//     components: {
+//       default: Page,
+//     }
+//   }, {
+//     path: "/first-form",
+//     name: "FirstForm",
+//     components: {
+//       default: FirstForm,
+//     }
+//   }, {
+//     path: "/not-found",
+//     name: "NotFound",
+//     components: {
+//       default: NotFound,
+//     }
+//   },]);
+// });
