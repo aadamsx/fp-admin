7e7f64eb9b5d2c79f4a1dffc32bfbc331faabe84
aadamsx <aadams99@gmail.com>
2017-01-03 19:03:05 -0600

    added Vue import

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 import '/imports/client/router.js';
-import { Vue } from 'meteor/akryum:vue';
+// import { Vue } from 'meteor/akryum:vue';
+import Vue from 'vue'
 import { Router, nativeScrollBehavior } from 'meteor/akryum:vue-router2';
 import AppLayout from '/imports/client/views/AppLayout.vue';
 
